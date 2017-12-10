df1ecedef0f6e9fadd5b7aa45884168957e3fa3c
alexgriff <alexalexgriffith@gmail.com>
2017-12-10 14:50:41 -0500

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,13 @@ export const addNote = text => ({
 });
 // NOTE: generally the id of a resource would come from our backend,
 // since we dont have that set up here, we can use a unique timestamp
-// to stand in for a unique Primary Key ID
+// to stand in for a unique Primary Key ID,
+// a note can look like
+// {
+//   text: 'some text',
+//   timestamp: Date.now(),
+//   id: Date.now()
+// }
 
 export const deleteNote = id => ({
   type: 'DELETE_NOTE',
