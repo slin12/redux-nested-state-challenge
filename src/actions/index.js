f80724f436f82036ff16711eca00c9b557822641
alexgriff <alexalexgriffith@gmail.com>
2017-12-10 15:04:33 -0500

    midway thru

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,6 @@
-export const addNote = text => ({
-  type: 'ADD_NOTE',
-  note: {
-    text,
-    timestamp: Date.now()
-  }
-});
+export const addNote = text => {
+  return {};
+};
 // NOTE: generally the id of a resource would come from our backend,
 // since we dont have that set up here, we can use a unique timestamp
 // to stand in for a unique Primary Key ID,
@@ -15,7 +11,6 @@ export const addNote = text => ({
 //   id: Date.now()
 // }
 
-export const deleteNote = id => ({
-  type: 'DELETE_NOTE',
-  id
-});
+export const deleteNote = id => {
+  return {};
+};
--- src/reducer.js
+++ src/reducer.js
@@ -2,14 +2,6 @@ const initialState = { notes: [] };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_NOTE':
-      return {
-        notes: [...state.notes, action.note]
-      };
-    case 'DELETE_NOTE':
-      return {
-        notes: state.notes.filter(note => note.timestamp !== action.id)
-      };
     default:
       return state;
   }
